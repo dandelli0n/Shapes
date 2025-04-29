c7c88db09d67ca0dd9bb6e9fc91736e7c0d953e8
dandelli0n <gyarmiviki@gmail.com>
2025-04-29 22:04:55 +0200

    made the skeleton and a few functions

---
+++ progHF/Shapes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vikiii on 04/29/2025.
+//
+
+#include "Shapes.hpp"
---
+++ progHF/Shapes.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Vikiii on 04/29/2025.
+//
+
+#ifndef PROGHF_SHAPES_HPP
+#define PROGHF_SHAPES_HPP
+
+
+class Shapes
+{
+
+};
+
+
+#endif //PROGHF_SHAPES_HPP
